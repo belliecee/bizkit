3fd49201acbb291656f23435f871febedd1c693e
unknown <chaloemchai@NB00102.etda.or.th>
2014-11-11 10:36:00 +0700

    new pricing table

---
+++ page_header.php
@@ -0,0 +1,44 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+
+
+        <link href="css/bizkitStyle.css" rel="stylesheet" type="text/css" /> 
+
+        <script type="text/javascript" src="js/jQuery.1.7.2.js" ></script>
+        <script type="text/javascript" src="js/jquery-ui.min.js" ></script>
+        <script type="text/javascript" src="js/jQueryUI.1.8.21.js"></script>
+       
+        <script type="text/javascript" src="js/AccessibleSlide_1.js"></script>
+         <link rel="shortcut icon" href="favicon.ico">
+
+        <!-- Fonts START -->
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|PT+Sans+Narrow|Source+Sans+Pro:200,300,400,600,700,900&amp;subset=all" rel="stylesheet" type="text/css">
+        <!-- Fonts END -->
+
+        <!-- Global styles START -->          
+        <link href="assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <link href="assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Global styles END --> 
+
+        <!-- Page level plugin styles START -->
+        <link href="assets/global/plugins/fancybox/source/jquery.fancybox.css" rel="stylesheet">
+        <link href="assets/global/plugins/carousel-owl-carousel/owl-carousel/owl.carousel.css" rel="stylesheet">
+        <link href="assets/global/plugins/slider-revolution-slider/rs-plugin/css/settings.css" rel="stylesheet">
+        <!-- Page level plugin styles END -->
+
+        <!-- Theme styles START -->
+        <link href="assets/global/css/components.css" rel="stylesheet">
+        <link href="assets/frontend/layout/css/style.css" rel="stylesheet">
+        <link href="assets/frontend/pages/css/style-revolution-slider.css" rel="stylesheet"><!-- metronic revo slider styles -->
+        <link href="assets/frontend/layout/css/style-responsive.css" rel="stylesheet">
+        <link href="assets/frontend/layout/css/themes/green.css" rel="stylesheet" id="style-color">
+        <link href="assets/frontend/layout/css/custom.css" rel="stylesheet">
+        <!-- Theme styles END -->
+
+  
---
+++ pricing_table02.php
@@ -0,0 +1,209 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
+ <link href="assets/frontend/layout/css/style.css" rel="stylesheet" type="text/css" />
+<link href="assets/frontend/layout/css/themes/green.css" rel="stylesheet" id="style-color">
+<style>
+    .col-md-3 {
+        width: 33%;
+        margin-top: 20px;
+    }
+    .pricing-head h3 {
+        background: none repeat scroll 0% 0% #0d83dd;
+    }
+    .pricing:hover, .pricing:active, .pricing-active{
+        border: 3px solid #0D83DD;
+    }
+    .price_emph{
+        
+        color: #0D83DD !important;
+        //font-size: 14px;
+        font: 400 13px "Open Sans","Arial","sans-serif";
+        min-width: 20px !important;
+        display: inline-block;
+       
+    }
+    .pricing-content li {
+        padding: 15px 15px;
+    }
+    .portlet > .portlet-title {
+        border-bottom: none;
+    }
+    
+</style>
+
+<!-- BEGIN CONTENT -->
+<div class="page-content-wrapper">
+        <div class="page-content">
+            
+                <!-- BEGIN PAGE CONTENT-->
+                <div class="row">
+                        <div class="col-md-12">
+                                <!-- BEGIN INLINE NOTIFICATIONS PORTLET-->
+                                <div class="portlet light">
+                                        <div class="portlet-title">
+                                                
+                                        </div>
+                                        <div class="portlet-body">
+                                                <div class="row margin-bottom-40">
+                                                        <!-- Pricing -->
+                                                        <div class="col-md-3">
+                                                                <div class="pricing hover-effect">
+                                                                        <div class="pricing-head">
+                                                                                <h3>Starter <span>
+                                                                                ผู้ประกอบการ SME ขนาดย่อม </span>
+                                                                                </h3>
+                                                                               <h4><i>฿</i>700<i>.00</i>
+                                                                                <span style="color:#777">
+                                                                                per Month </span>
+                                                                                </h4>
+                                                                        </div>
+                                                                        <ul class="pricing-content list-unstyled">
+                                                                                <li>
+																					
+                                                                                    <i class="fa fa-tags"></i>  รายชื่อลูกค้าสูงสุด <span class="price_emph">7 รายชื่อ</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa fa-users"></i> จำนวนเพื่อนร่วมทีม <span class="price_emph">N/A</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-file-text-o"></i> จำนวน Invoice <span class="price_emph">ไม่จำกัด</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-star"></i> ผู้รับจ้างที่ออก Invoice ได้ <span class="price_emph">50 บัญชี</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-unlock"></i> ซ่อน logo ของ Bizkit <span class="price_emph">Support</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                       <i class="fa fa-shopping-cart"></i> ติดตามการใช้จ่ายอัติโนมัติ <span class="price_emph">Support</span>
+                                                                                </li>
+                                                                               
+
+                                                                        </ul>
+                                                                        <div class="pricing-footer">
+                                                                                <p>
+                                                                                          <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                                                                            <input type="hidden" name="cmd" value="_s-xclick">
+                                                                                            <input type="hidden" name="hosted_button_id" value="3EJ2D3FZGKJL8">
+                                                                                            <input type="image" src="https://www.paypalobjects.com/en_GB/TH/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+                                                                                            <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                                                                                        </form>
+                                                                                </p>
+                                                                                
+                                                                        </div>
+                                                                </div>
+                                                        </div>
+                                                       
+                                                        <div class="col-md-3">
+                                                                <div class="pricing  hover-effect">
+                                                                        <div class="pricing-head pricing-head-active">
+                                                                                <h3>Business <span>
+                                                                                 ผู้ประกอบการ SME ขนาดกลาง </span>
+                                                                                </h3>
+                                                                                <h4><i>฿</i>900<i>.00</i>
+                                                                                 <span style="color:#777">
+                                                                                per Month </span>
+                                                                                </h4>
+                                                                        </div>
+                                                                        <ul class="pricing-content list-unstyled">
+                                                                                <li>
+                                                                                        <i class="fa fa-tags"></i> จัดเก็บรายชื่อลูกค้า <span class="price_emph">25 รายชื่อ</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa fa-users"></i> จำนวนเพื่อนร่วมทีม <span class="price_emph">1 บัญชี</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                       <i class="fa fa-file-text-o"></i> จำนวน Invoice <span class="price_emph">ไม่จำกัด</span>
+                                                                                </li>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-star"></i> ผู้รับจ้างที่ออก Invoice ได้ <span class="price_emph">100 บัญชี</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                       <i class="fa fa-unlock"></i> ซ่อน logo ของ Bizkit <span class="price_emph">Support</span>
+                                                                                </li>
+                                                                                 <li>
+                                                                                        <i class="fa fa-shopping-cart"></i> ติดตามการใช้จ่ายอัติโนมัติ <span class="price_emph">Support</span>
+                                                                                </li>
+                                                                        </ul>
+                                                                        <div class="pricing-footer">
+                                                                                <p>
+                                                                                         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                                                                            <input type="hidden" name="cmd" value="_s-xclick">
+                                                                                            <input type="hidden" name="hosted_button_id" value="H4SFEHZUDM5WL">
+                                                                                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                                                                                            <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                                                                                        </form>
+                                                                                </p>
+                                                                                
+                                                                        </div>
+                                                                </div>
+                                                        </div>
+                                                        <div class="col-md-3">
+                                                                <div class="pricing hover-effect">
+                                                                        <div class="pricing-head">
+                                                                                <h3>Premium <span>
+                                                                                 ผู้ประกอบการรายใหญ่   </span>
+                                                                                </h3>
+                                                                                <h4><i>฿</i>1200<i>.00</i>
+                                                                                 <span style="color:#777">
+                                                                                per Month </span>
+                                                                                </h4>
+                                                                        </div>
+                                                                        <ul class="pricing-content list-unstyled">
+                                                                                <li>
+                                                                                        <i class="fa fa-tags"></i>  จัดเก็บรายชื่อลูกค้า <span class="price_emph">ไม่จำกัด</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-users"></i>จำนวนเพื่อนร่วมทีม <span class="price_emph">3 บัญชี</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-file-text-o"></i> จำนวน Invoice <span class="price_emph">ไม่จำกัด</span>
+                                                                                </li>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-star"></i> ผู้รับจ้างที่ออก Invoice ได้ <span class="price_emph">ไม่จำกัด</span>
+                                                                                </li>
+                                                                                <li>
+                                                                                        <i class="fa fa-unlock"></i> ซ่อน logo ของ Bizkit <span class="price_emph">Support</span>
+                                                                                </li>
+                                                                                 <li>
+                                                                                        <i class="fa fa-shopping-cart"></i> ติดตามการใช้จ่ายอัติโนมัติ <span class="price_emph">Support</span>
+                                                                                <!--</li>-->
+                                                                        </ul>
+                                                                        <div class="pricing-footer">
+                                                                                <p>
+                                                                                            <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                                                                                                <input type="hidden" name="cmd" value="_s-xclick">
+                                                                                                <input type="hidden" name="hosted_button_id" value="RHPWMJLPRV8G2">
+                                                                                                <input type="image" src="https://www.paypalobjects.com/en_GB/TH/i/btn/btn_buynow_LG.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online.">
+                                                                                                <img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+                                                                                                </form>
+
+
+                                                                                </p>
+                                                                               
+                                                                        </div>
+                                                                </div>
+                                                        </div>
+                                                        
+														
+														<!--//End Pricing -->
+                                                </div>
+                                        </div>
+                                </div>
+                                <!-- END INLINE NOTIFICATIONS PORTLET-->
+                        </div>
+                </div>
+                <!-- END PAGE CONTENT-->
+        </div>
+        <!-- BEGIN CONTENT -->
+</div>
+<!-- END CONTAINER -->
